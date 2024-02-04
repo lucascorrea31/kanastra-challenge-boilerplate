86b87cd15aa7387a620d04efe86cdb83e3fb5e2e
Lucas Corrêa <lucas.nhl@gmail.com>
2024-02-03 23:07:04 -0300

    🔧 add logo component

---
+++ src/components/ui/logo.tsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+  return (
+    <div className="p-4 w-fit h-fit">
+      <img src="/src/assets/logo.png" alt="Logo" className="max-w-xs" />
+    </div>
+  );
+};
+
+export default Logo;
