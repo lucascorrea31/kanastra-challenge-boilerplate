a60e7a3371231ade0e00bbc38a6e77fb18ef559b
Lucas Corrêa <lucas.nhl@gmail.com>
2024-02-03 23:08:24 -0300

    🔧 add helper functions to convert data to an human reading type

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,35 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function humanSize(bytes: number) {
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+  if (bytes === 0) return "0 Byte";
+  const i = Math.floor(Math.log(bytes) / Math.log(1024));
+  return `${(bytes / Math.pow(1024, i)).toFixed(2)} ${sizes[i]}`;
+}
+
+export function humanDate(date: Date) {
+  return new Date(date).toLocaleString();
+}
+
+const mimeTypes = {
+  "application/pdf": "PDF",
+  "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+    "Word",
+  "application/msword": "Word",
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": "Excel",
+  "application/vnd.ms-excel": "Excel",
+  "application/vnd.ms-powerpoint": "PowerPoint",
+  "application/vnd.openxmlformats-officedocument.presentationml.presentation":
+    "PowerPoint",
+  "text/plain": "Text",
+  "text/csv": "CSV",
+};
+
+export function humanFileType(type: string) {
+  return mimeTypes[type as keyof typeof mimeTypes] || "File";
 }
